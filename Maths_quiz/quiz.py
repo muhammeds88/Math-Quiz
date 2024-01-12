6dedc9463467d00caf93568085b89eba96277ff8
MUHAMMED.S <147074256+muhammeds88@users.noreply.github.com>
2024-01-12 12:36:41 -0500

    Update quiz.py

--- Maths_quiz/quiz.py
+++ Maths_quiz/quiz.py
@@ -1,26 +1,29 @@
 import random
 import time
 
+# Get user's name input
 name = input('Enter your Name: ')
 
 while True:
     def grade():
+        # Evaluate and print the grade based on the number of correct answers
         if correct >= 8:
             print('Excellent! You got A grade.')
-        elif correct > 5 and correct < 8:
+        elif 5 < correct < 8:
             print("Good! You got B grade.")
-        elif correct > 3 and correct < 5:
+        elif 3 < correct < 5:
             print("Need Improvement! You got C grade.")
         elif correct <= 3:
             print('Very Bad! You got D grade.')
 
 
-    SIGN = ['+', '-', '*']  # these are constants and cannot change
+    # Constants for the math quiz
+    SIGN = ['+', '-', '*']
     MIN_NUM = 1
     MAX_NUM = 10
     TOTAL_PROBLEMS = 10
 
-
+    # Function to create a math problem
     def create_problem():
         left_num = random.randint(MIN_NUM, MAX_NUM)
         right_num = random.randint(MIN_NUM, MAX_NUM)
@@ -31,21 +34,22 @@ while True:
         answer = eval(exprmnt)
         return exprmnt, answer
 
-
+    # Variables to track correct and wrong answers
     wrong = 0
     correct = 0
 
+    # Introduction for the sample test
     print('\nSample Test')
-
     print(f'Hello {name} welcome to Maths Test.\nYou have total 10 questions to be done in Sample Test.\n'
           f'----------------------------\nNumbers included = [1 - 10]\nSigns included = [+, -, *]\n'
           f'----------------------------\nThe time taken to complete the task will be shown')
     print("All the Best!")
-    input('Press the Enter to Start.')
+    input('Press Enter to Start.')
     print('-------------------------')
 
     start_time = time.time()
 
+    # Loop through total problems for the sample test
     for i in range(TOTAL_PROBLEMS):
         exprmnt, answer = create_problem()
         while True:
@@ -54,29 +58,32 @@ while True:
                 correct += 1
                 break
             else:
-                print("wrong answer.")
+                print("Wrong answer.")
                 wrong += 1
                 break
 
             i += 1
 
     finish_time = time.time()
-    total_time = round(finish_time - start_time, 3)  # round the numbers in to 3 digits
+    total_time = round(finish_time - start_time, 3)
 
+    # Display results and grade for the sample test
     print(f'You got {correct} correct answers and {wrong} wrong answers')
     grade()
 
     print("You have finished in: ", total_time, 'seconds')
 
-    again = input("\nDo you wanna continue to the next level?(y or n) ")
-    if again == 'y':
-        print('Good\nSelect the level')
-    else:
+    # Ask if the user wants to continue to the next level
+    again = input("\nDo you want to continue to the next level? (y or n) ")
+    if again != 'y':
         quit()
+
+    # Prompt the user to select the difficulty level
     mode = input("Enter 'e' for Easy\nEnter 'h' for Hard\n")
 
     if mode == 'e':
         def gradee():
+            # Evaluate and print the grade for the Easy level
             if correct > 4:
                 print('Excellent! You got A grade.')
             elif correct == 4:
@@ -86,27 +93,17 @@ while True:
             elif correct <= 2:
                 print('Very Bad! You got D grade.')
 
-
+        # Update constants for the Easy level
         MIN_NUM = 0
         MAX_NUM = 5
         SIGN = ['+', '-']
         TOTAL_PROBLEMS = 5
 
-
-        def create_problem():
-            left_num = random.randint(MIN_NUM, MAX_NUM)
-            right_num = random.randint(MIN_NUM, MAX_NUM)
-            operator = random.choice(SIGN)
-
-            exprmnt = str(left_num) + ' ' + operator + ' ' + str(right_num)
-
-            answer = eval(exprmnt)
-            return exprmnt, answer
-
-
+        # Reset variables for the Easy level
         wrong = 0
         correct = 0
 
+        # Introduction for the Easy level
         print('**********************************************')
         print('Level: Easy\n')
         print(
@@ -115,15 +112,16 @@ while True:
             'Signs included = [+, -]\n----------------------------\n'
             'The time taken to complete the task will be shown')
         print("All the Best!")
-        input('Press the Enter to Start.')
+        input('Press Enter to Start.')
         print('-------------------------')
 
         start_time = time.time()
 
+        # Loop through total problems for the Easy level
         for i in range(TOTAL_PROBLEMS):
             exprmnt, answer = create_problem()
             while True:
-                guess = input('Problem #' + str(i + 1) + ' : ' +  exprmnt + ' = ')
+                guess = input('Problem #' + str(i + 1) + ' : ' + exprmnt + ' = ')
                 if guess == str(answer):
                     correct += 1
                     break
@@ -137,6 +135,7 @@ while True:
         finish_time = time.time()
         total_time = round(finish_time - start_time, 3)
 
+        # Display results and grade for the Easy level
         print(f'You got {correct} correct answers and {wrong} wrong answers')
         gradee()
 
@@ -145,34 +144,27 @@ while True:
 
     if mode == 'h':
         def gradeh():
+            # Evaluate and print the grade for the Hard level
             if correct > 11 or correct == 11:
                 print('Excellent! You got A grade.')
-            elif correct > 7 and correct < 11:
+            elif 7 < correct < 11:
                 print("Good! You got B grade.")
-            elif correct > 3 and correct < 5:
+            elif 3 < correct < 5:
                 print("Need Improvement! You got C grade.")
             elif correct < 3 or correct == 3:
                 print('Very Bad! You got D grade.')
 
-
-        SIGN = ['+', '-', '*']  # these are constants and cannot change
+        # Update constants for the Hard level
+        SIGN = ['+', '-', '*']
         MIN_NUM = 1
         MAX_NUM = 30
         TOTAL_PROBLEMS = 15
 
-
-        def create_problem():
-            left_num = random.randint(MIN_NUM, MAX_NUM)
-            right_num = random.randint(MIN_NUM, MAX_NUM)
-            operator = random.choice(SIGN)
-
-            exprmnt = str(left_num) + ' ' + operator + ' ' + str(right_num)
-
-            answer = eval(exprmnt)
-            return exprmnt, answer
+        # Reset variables for the Hard level
         wrong = 0
         correct = 0
 
+        # Introduction for the Hard level
         print('**********************************************')
         print('Level: Hard\n')
         print(
@@ -180,11 +172,12 @@ while True:
             '----------------------------\nNumbers included = [1 - 30]\nSigns included = [+, -, *]\n'
             '----------------------------\nThe time taken to complete the task will be shown')
         print("All the Best!")
-        input('Press the Enter to Start.')
+        input('Press Enter to Start.')
         print('-------------------------')
 
         start_time = time.time()
 
+        # Loop through total problems for the Hard level
         for i in range(TOTAL_PROBLEMS):
             exprmnt, answer = create_problem()
             while True:
@@ -193,15 +186,16 @@ while True:
                     correct += 1
                     break
                 else:
-                    print("wrong answer.")
+                    print("Wrong answer.")
                     wrong += 1
                     break
 
                 i += 1
 
         finish_time = time.time()
-        total_time = round(finish_time - start_time, 3)  # round the numbers in to 3 digits
+        total_time = round(finish_time - start_time, 3)
 
+        # Display results and grade for the Hard level
         print(f'You got {correct} correct answers and {wrong} wrong answers')
         gradeh()
 
